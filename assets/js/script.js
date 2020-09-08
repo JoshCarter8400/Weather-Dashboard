d13dd392003d8e56b30504ca4a4c03916f042f81
Josh Carter <joshcarter8400@gmail.com>
2020-09-08 11:09:12 -0400

    added api fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
 var getWeatherInfo = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?id=6167865&appid=8d1c76621b3c56e2c6ba4131cbdbfec9";
 
+    // make a repo request to the url
+    fetch(apiUrl).then(function(repsonse) {
+        repsonse.json().then(function(data) {
+            console.log(data)
+        });
+    });
 }
 getWeatherInfo()

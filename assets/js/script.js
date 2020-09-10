5bef158bdb11c69447b442a7e1cdfb1029870dbd
Josh Carter <joshcarter8400@gmail.com>
2020-09-10 07:38:28 -0400

    adding city lists

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,28 @@
 var searchInput = document.querySelector("#search-input")
 var findCity = document.querySelector("#search-btn")
-
+var listItemE1 = document.querySelector(".list-group-item")
+var cityCard = document.querySelector("#city-container")
 
 
 var getWeatherInfo = function(user) {
 
     // format the github api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?id=6167865&appid=8d1c76621b3c56e2c6ba4131cbdbfec9";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + user + "&appid=8d1c76621b3c56e2c6ba4131cbdbfec9";
 
     // make a repo request to the url
     fetch(apiUrl).then(function(repsonse) {
         repsonse.json().then(function(data) {
-            displayWeather(data, user);
+            // displayWeather(data, user);
+            console.log(data)
         });
     });
 };
-getWeatherInfo();
+
 
 
 var searchHandler = function(event) {
     var weather = searchInput.value.trim();
-
+    console.log(weather)
     if (weather) {
         getWeatherInfo(weather);
         searchInput.value = "";
@@ -33,5 +35,13 @@ var searchHandler = function(event) {
 var displayWeather = function(weather, searchInfo) {
     console.log(weather)
     console.log(searchInfo)
+}
+
+var cityHistory = function(cityList, showCity) {
+    // clear old search
+    cityCard.textContent = "";
+    searchInput.textContent = showCity
+
+    listItemE1.appendChild(cityCard)
 }
 findCity.addEventListener("click", searchHandler);
